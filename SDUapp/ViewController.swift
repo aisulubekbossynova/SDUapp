1bb88532029b7f07785abab7e9f3bf7ebaa15279
aisulubekbossynova <aisulu175@gmail.com>
2018-06-02 19:26:14 +0600

    sdu application ios

--- SDUapp/ViewController.swift
+++ SDUapp/ViewController.swift
@@ -2,24 +2,39 @@
 //  ViewController.swift
 //  SDUapp
 //
-//  Created by Macbook on 27.02.18.
+//  Created by Macbook on 02.03.18.
 //  Copyright © 2018 Macbook. All rights reserved.
 //
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UISplitViewController , UISplitViewControllerDelegate{
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        self.delegate = self
+        // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func splitViewController(splitViewController: UISplitViewController, collapseSecondaryViewController secondaryViewController: UIViewController!, ontoPrimaryViewController primaryViewController: UIViewController!) -> Bool{
+        return true
+    }
+    
 
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
 
 }
-
